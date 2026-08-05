1cbfb50353c76b3eae42e03c3478c1c46cada8f8
Santiago Gomez <Santigomez1129@gmail.com>
2026-08-05 18:19:29 -0300

    TierRow component added

---
+++ src/components/TierRow.tsx
@@ -0,0 +1,29 @@
+import RankingItemCard from "./RankingItemCard";
+
+type TierRowProps = {
+  name: string;
+  id: string;
+  items: {
+    id: string;
+    title: string;
+  }[];
+};
+
+export default function TierRow({ id, name, items }: TierRowProps) {
+  return (
+    <div
+      key={id}
+      className="grid grid-cols-[7rem_minmax(0,1fr)] border-b border-app-border last:border-b-0"
+    >
+      <div className="flex min-h-20 items-center justify-center border-r-2 border-app-border p-2">
+        <span className="app-title text-2xl">{name}</span>
+      </div>
+
+      <div className="grid min-w-0 grid-cols-[repeat(auto-fill,minmax(7rem,1fr))] gap-0 bg-app-bg-soft">
+        {items.map((item: { id: string; title: string }) => (
+          <RankingItemCard key={`${id}-${item.id}`} title={item.title} />
+        ))}
+      </div>
+    </div>
+  );
+}
--- src/pages/TemplateRankingPage.tsx
+++ src/pages/TemplateRankingPage.tsx
@@ -2,6 +2,7 @@ import { useParams } from "react-router";
 import { templates } from "../features/templates/templateData";
 import { NotFoundPage } from "./NotFoundPage";
 import RankingItemCard from "../components/RankingItemCard";
+import TierRow from "../components/TierRow";
 
 export function TemplateRankingPage() {
   const { id } = useParams();
@@ -29,23 +30,7 @@ export function TemplateRankingPage() {
         <section className="app-section flex w-full flex-col items-center gap-5 p-5">
           <div className="w-full max-w-5xl overflow-hidden rounded-2xl border border-app-border">
             {template.tiers.map((tier) => (
-              <div
-                key={tier.id}
-                className="grid grid-cols-[7rem_minmax(0,1fr)] border-b border-app-border last:border-b-0"
-              >
-                <div className="flex min-h-20 items-center justify-center border-r-2 border-app-border p-2">
-                  <span className="app-title text-2xl">{tier.name}</span>
-                </div>
-
-                <div className="grid min-w-0 grid-cols-[repeat(auto-fill,minmax(7rem,1fr))] gap-0 bg-app-bg-soft">
-                  {tier.items.map((item: { id: string; title: string }) => (
-                    <RankingItemCard
-                      key={`${tier.id}-${item.id}`}
-                      title={item.title}
-                    />
-                  ))}
-                </div>
-              </div>
+              <TierRow name={tier.name} id={tier.id} items={tier.items} />
             ))}
           </div>
 
